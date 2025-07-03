d9296542eb2f958380c3a56624485b537bac5c05
FerryLa <rlawl4240@gmail.com>
2025-07-03 10:41:42 +0900

    modify: CalculatorController file : ArgoCD Test

--- src/main/java/com/ohgiraffers/jenkins/contoller/CalculatorController.java
+++ src/main/java/com/ohgiraffers/jenkins/contoller/CalculatorController.java
@@ -18,7 +18,7 @@ public class CalculatorController {
 
     @GetMapping("/health")
     public String healthCheck() {
-        return "I'm alive!🗝🥛";
+        return "ArgoCD Settings: 🐵🐋";
     }
 
     @GetMapping("/plus")
